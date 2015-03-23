f8e037856b2e0ec93d2c5a6b5a425fa1ab0fb6c6
Derek Menteer <leechy9@gmail.com>
2015-03-22 23:10:36 -0500

    Made admin interface more readable

--- soniferous/music_player/admin.py
+++ soniferous/music_player/admin.py
@@ -7,6 +7,12 @@ class SongAdmin(admin.ModelAdmin):
   New model: Displays an upload field for MP3 files.
   Change model: Displays text fields to modify existing data.
   '''
+  list_display = ('title', 'album_', 'artist_',)
+  ordering = ('album__album', 'album__artist__artist', 'track_number', 'title',)
+  def album_(self, song):
+    return song.album.album
+  def artist_(self, song):
+    return song.album.artist.artist
   def get_form(self, request, obj=None, **kwargs):
     if obj:
       self.form = ModifySongForm
